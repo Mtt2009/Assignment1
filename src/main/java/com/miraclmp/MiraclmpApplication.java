13e5ecbdc07938a3d746455586e06c9231745597
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-25 19:22:58 -0400

    Inital Commit of Assignment

---
+++ src/main/java/com/miraclmp/MiraclmpApplication.java
@@ -0,0 +1,13 @@
+package com.miraclmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiraclmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiraclmpApplication.class, args);
+	}
+
+}
